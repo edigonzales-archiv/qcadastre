53261cb05d39e5f470495d2e6a3c5816fba80001
Stefan Ziegler <stefan.ziegler.de@gmail.com>
2013-10-21 22:10:28 +0200

    init_db shell skript angepasst

--- tmp/init_db/01_init.sh
+++ tmp/init_db/01_init.sh
@@ -1,16 +1,18 @@
-ADMIN="bjsvwzieadmin"
-ADMINPWD="bjsvwzieadmin"
-USER="bjsvwziepublic"
-USERPWD="bjsvwziepublic"
-DB_NAME="pnf_bjsvwzie"
+#!/bin/bash
 
-GROUP="pnfpublic";
+ADMIN="stefan"
+ADMINPWD="ziegler12"
+USER="mspublic"
+USERPWD="mspublic"
+
+DB_NAME="xanadu2"
 PG_VERSION="9.1"
+POSTGIS_VERSION="postgis-2.1"
 
 # Auskommentieren falls man z.B. nur DB neu anlegen will.
 echo "Create database user"
-sudo -u postgres psql -d postgres -c "CREATE ROLE $ADMIN CREATEDB LOGIN PASSWORD '$ADMINPWD';"
-sudo -u postgres psql -d postgres -c "CREATE ROLE $USER LOGIN PASSWORD '$USERPWD';"
+#sudo -u postgres psql -d postgres -c "CREATE ROLE $ADMIN CREATEDB LOGIN PASSWORD '$ADMINPWD';"
+#sudo -u postgres psql -d postgres -c "CREATE ROLE $USER LOGIN PASSWORD '$USERPWD';"
 
 echo "Create database: $DB_NAME"
 sudo -u postgres createdb --owner $ADMIN $DB_NAME
@@ -18,8 +20,9 @@ sudo -u postgres createlang plpgsql $DB_NAME
 sudo -u postgres psql -d $DB_NAME -c "ALTER SCHEMA public OWNER TO $ADMIN;"
 
 echo "Load postgis"
-sudo -u postgres psql -d $DB_NAME -f /usr/share/postgresql/$PG_VERSION/contrib/postgis-1.5/postgis.sql
-sudo -u postgres psql -d $DB_NAME -f /usr/share/postgresql/$PG_VERSION/contrib/postgis-1.5/spatial_ref_sys.sql
+sudo -u postgres psql -d $DB_NAME -f /usr/share/postgresql/$PG_VERSION/contrib/$POSTGIS_VERSION/postgis.sql
+sudo -u postgres psql -d $DB_NAME -f /usr/share/postgresql/$PG_VERSION/contrib/$POSTGIS_VERSION/postgis_comments.sql
+sudo -u postgres psql -d $DB_NAME -f /usr/share/postgresql/$PG_VERSION/contrib/$POSTGIS_VERSION/spatial_ref_sys.sql
 
 echo "Grant tables to..."
 sudo -u postgres psql -d $DB_NAME -c "GRANT ALL ON SCHEMA public TO $ADMIN;"
@@ -32,5 +35,5 @@ sudo -u postgres psql -d $DB_NAME -c "GRANT SELECT ON geometry_columns TO $USER;
 sudo -u postgres psql -d $DB_NAME -c "GRANT SELECT ON spatial_ref_sys TO $USER;"
 sudo -u postgres psql -d $DB_NAME -c "GRANT SELECT ON geography_columns TO $USER;"
 
-sudo -u postgres psql -d $DB_NAME -c "GRANT $GROUP TO $ADMIN;"
-sudo -u postgres psql -d $DB_NAME -c "GRANT $GROUP TO $USER;"
+
+
